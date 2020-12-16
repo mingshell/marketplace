f12a786529fd7a1d8640f734246bd2de1c5e7e80
Miguel Escobar <ming1shell@gmail.com>
2020-12-16 12:03:55 -0600

    configuracion del archivo Procfile

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   console.log('Sever running: http://127.0.0.1:' + AppModule.port);
-  await app.listen(AppModule.port);
+  await app.listen(AppModule.port || 3000);
 }
 bootstrap();
